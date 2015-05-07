a0d1f0391674b4f20b47249260dcd6c3863ae745
Dyon Enedi <dyonenedi@hotmail.com>
2015-05-06 21:11:21 -0300

    Adicionando arquivos

---
+++ dyonenedi/block.php
@@ -0,0 +1,20 @@
+<?php
+	
+	namespace Plugin\Dyonenedi;
+
+	Class Block
+	{
+		public static function mount($data, $replace, $html){
+			$list = '';
+			if (!empty($replace) && is_array($replace)) {
+				foreach ($data as $row) {
+					$list .= $html;
+					foreach ($replace as $key => $value) {
+						$list = str_replace('<%'.strtoupper($key).'%>', $row[$value], $list);
+					}
+				}
+			}
+
+			return $list;
+		}
+	}
